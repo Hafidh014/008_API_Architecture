0f7f12c13cf7dcf74071a552cf71da270486b2d6
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-07-22 15:45:23 +0700

    Routes GET

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const komikController = require('../controller/komikController');
+
+router.get('/komik', komikController.getAllKomik);
+router.get('/komik/:id', komikController.getKomikById);
+
+
