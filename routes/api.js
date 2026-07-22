80e982bab3cfd138527a451c78032270cc2574fa
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-07-22 15:46:09 +0700

    post,put dan delete

--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const komikController = require('../controller/komikController');
 
 router.get('/komik', komikController.getAllKomik);
 router.get('/komik/:id', komikController.getKomikById);
-
-
+router.post('/komik', komikController.createKomik);
+router.put('/komik/:id', komikController.updateKomik);
+router.delete('/komik/:id', komikController.deleteKomik);
